e9a7af1ad3e9b537b3247ad510bc0012664c8af6
Dzuchun <Infernodambik@gmail.com>
2024-11-20 04:44:52 +0200

    Linear model

---
+++ nacfahi/src/models/basic/linear.rs
@@ -0,0 +1,48 @@
+use core::ops::{Add, Mul};
+
+use generic_array::GenericArray;
+use num_traits::One;
+
+use typenum::U2;
+
+use crate::models::FitModel;
+
+/// Line model $a \cdot x + b$
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Linear<Scalar> {
+    /// Line tangent, $a$
+    pub a: Scalar,
+    /// Line offset, $b$
+    pub b: Scalar,
+}
+
+impl<Scalar: Clone + Add<Output = Scalar> + Mul<Output = Scalar> + One> FitModel<Scalar>
+    for Linear<Scalar>
+{
+    type ParamCount = U2;
+
+    #[inline]
+    fn evaluate(&self, x: &Scalar) -> Scalar {
+        self.a.clone() * x.clone() + self.b.clone()
+    }
+
+    #[inline]
+    fn jacobian(&self, x: &Scalar) -> impl Into<GenericArray<Scalar, Self::ParamCount>> {
+        // y = a * x + b
+        // - derivative over a is x
+        // - derivative over b is 1
+        [x.clone(), Scalar::one()]
+    }
+
+    #[inline]
+    fn set_params(&mut self, new_params: GenericArray<Scalar, Self::ParamCount>) {
+        let [new_a, new_b] = new_params.into_array();
+        self.a = new_a;
+        self.b = new_b;
+    }
+
+    #[inline]
+    fn get_params(&self) -> impl Into<GenericArray<Scalar, Self::ParamCount>> {
+        [self.a.clone(), self.b.clone()]
+    }
+}
--- nacfahi/src/models/basic/mod.rs
+++ nacfahi/src/models/basic/mod.rs
@@ -4,7 +4,10 @@ mod constant;
 mod exponent;
 #[doc(hidden)]
 mod gaussian;
+#[doc(hidden)]
+mod linear;
 
 pub use constant::*;
 pub use exponent::*;
 pub use gaussian::*;
+pub use linear::*;
