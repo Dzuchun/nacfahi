84bf30355f5f30626188a32207cf39941dfa000a
Dzuchun <Infernodambik@gmail.com>
2024-11-20 04:40:29 +0200

    Exponent model

---
+++ nacfahi/src/models/basic/exponent.rs
@@ -0,0 +1,49 @@
+use core::ops::Mul;
+
+use num_traits::{FloatConst, Pow};
+
+use crate::models::FitModel;
+
+/// Exponent model $a \cdot \exp(b \cdot x )$
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Exponent<Scalar> {
+    /// Constant in front of the exponent
+    pub a: Scalar,
+    /// Exponent multiplier
+    pub b: Scalar,
+}
+
+impl<Scalar: Clone + Mul<Output = Scalar> + Pow<Scalar, Output = Scalar> + FloatConst>
+    FitModel<Scalar> for Exponent<Scalar>
+{
+    type ParamCount = typenum::U2;
+
+    #[inline]
+    fn evaluate(&self, x: &Scalar) -> Scalar {
+        self.a.clone() * (Scalar::E().pow(self.b.clone() * x.clone()))
+    }
+
+    #[inline]
+    fn jacobian(
+        &self,
+        x: &Scalar,
+    ) -> impl Into<generic_array::GenericArray<Scalar, Self::ParamCount>> {
+        // y = a * exp(bx)
+        // - derivative over a is exp(bx)
+        // - derivative over b is ax * exp(bx)
+        let e_x = Scalar::E().pow(self.b.clone() * x.clone());
+        [e_x.clone(), self.a.clone() * x.clone() * e_x]
+    }
+
+    #[inline]
+    fn set_params(&mut self, new_params: generic_array::GenericArray<Scalar, Self::ParamCount>) {
+        let [new_a, new_b] = new_params.into_array();
+        self.a = new_a;
+        self.b = new_b;
+    }
+
+    #[inline]
+    fn get_params(&self) -> impl Into<generic_array::GenericArray<Scalar, Self::ParamCount>> {
+        [self.a.clone(), self.b.clone()]
+    }
+}
--- nacfahi/src/models/basic/mod.rs
+++ nacfahi/src/models/basic/mod.rs
@@ -1,4 +1,7 @@
 #[doc(hidden)]
 mod constant;
+#[doc(hidden)]
+mod exponent;
 
 pub use constant::*;
+pub use exponent::*;
